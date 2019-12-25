83e33991433c02cbf7f79b375accbddd66d36f86
DemoHn <demohn2016@gmail.com>
2019-12-26 00:26:49 +0800

    feat: var assign grammer

---
+++ syntax/parser.go
@@ -0,0 +1,114 @@
+package syntax
+
+import (
+	"github.com/DemoHn/Zn/error"
+	"github.com/DemoHn/Zn/lex"
+)
+
+// Node - general node type
+type Node interface {
+	getType() nodeType
+}
+
+// Statement - a program consists of statements
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression - a special type of statement
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type nodeType int
+
+// ProgramNode - the syntax tree of a program
+type ProgramNode struct {
+	Children []Statement
+}
+
+func (p *ProgramNode) getType() nodeType {
+	return TypeProgram
+}
+
+// IdentifierNode - parse as normal identifier
+// (including varQuote or normal quote)
+type IdentifierNode struct {
+	literal string
+}
+
+func (in *IdentifierNode) getType() nodeType {
+	return TypeIdentifier
+}
+
+// declare node types
+const (
+	TypeProgram    nodeType = 0
+	TypeVarAssign  nodeType = 1 // 令...为...
+	TypeIdentifier nodeType = 2
+)
+
+// Parser - parse all nodes
+type Parser struct {
+	lexer        *lex.Lexer
+	currentToken *lex.Token
+	peekToken    *lex.Token
+}
+
+// NewParser -
+func NewParser(l *lex.Lexer) *Parser {
+	p := &Parser{
+		lexer: l,
+	}
+	// read current and peek token
+	p.next()
+	p.next()
+	return p
+}
+
+func (p *Parser) next() *error.Error {
+	tk, err := p.lexer.NextToken()
+	if err != nil {
+		return err
+	}
+
+	p.currentToken = p.peekToken
+	p.peekToken = tk
+	return nil
+}
+
+func (p *Parser) current() *lex.Token {
+	return p.currentToken
+}
+
+func (p *Parser) peek() *lex.Token {
+	return p.peekToken
+}
+
+// consume one token (without callback), will return error if the incoming token (p.currentToken)
+// is not in validTypes
+func (p *Parser) consume(validTypes ...lex.TokenType) *error.Error {
+	tk := p.current()
+	tkType := tk.Type
+	for _, item := range validTypes {
+		if item == tkType {
+			return p.next()
+		}
+	}
+	return error.NewErrorSLOT("syntax error")
+}
+
+// consume one token with error func
+func (p *Parser) consumeFunc(callback func(*lex.Token), validTypes ...lex.TokenType) *error.Error {
+	tk := p.current()
+	tkType := tk.Type
+	for _, item := range validTypes {
+		if item == tkType {
+			callback(tk)
+			return p.next()
+		}
+	}
+	return error.NewErrorSLOT("syntax error")
+}
---
+++ syntax/var_assign.go
@@ -0,0 +1,86 @@
+package syntax
+
+import (
+	"github.com/DemoHn/Zn/error"
+	"github.com/DemoHn/Zn/lex"
+)
+
+// VarAssignNode - var assignment statement
+type VarAssignNode struct {
+	Variables  []*IdentifierNode
+	AssignExpr Expression
+}
+
+func (vn *VarAssignNode) getType() nodeType {
+	return TypeVarAssign
+}
+
+// parsing process
+
+// ParseVarAssign - yield VarAssign node
+// CFG:
+// VarAssign -> 令 IdfList 为 Expr
+//   IdfList -> I I'
+//        I' -> ，I I'
+//           ->
+//
+func (p *Parser) ParseVarAssign() (*VarAssignNode, *error.Error) {
+	// #0. consume LING keyword
+	if err := p.consume(lex.TypeDeclareW); err != nil {
+		return nil, err
+	}
+
+	vNode := &VarAssignNode{
+		Variables:  make([]*IdentifierNode, 0),
+		AssignExpr: nil,
+	}
+	// #1. consume identifier list
+	if err := parseIdentifierList(p, vNode); err != nil {
+		return nil, err
+	}
+	// #2. consume logicYes
+	if err := p.consume(lex.TypeLogicYesW); err != nil {
+		return nil, err
+	}
+	// #3. TODO: parse expression
+	return vNode, nil
+}
+
+func parseIdentifierList(p *Parser, vNode *VarAssignNode) *error.Error {
+	// #0. consume Identifier
+	if err := p.consumeFunc(cbIdentifier(vNode), lex.TypeVarQuote, lex.TypeIdentifier); err != nil {
+		return err
+	}
+	// #1. parse identifier tail
+	if err := parseIdentifierTail(p, vNode); err != nil {
+		return err
+	}
+	return nil
+}
+
+func parseIdentifierTail(p *Parser, vNode *VarAssignNode) *error.Error {
+	// skip all parsing
+	if p.current().Type != lex.TypeCommaSep {
+		return nil
+	}
+	// #0. consume comma
+	if err := p.consume(lex.TypeCommaSep); err != nil {
+		return err
+	}
+	// #1. consume Identifier
+	if err := p.consumeFunc(cbIdentifier(vNode), lex.TypeVarQuote, lex.TypeIdentifier); err != nil {
+		return err
+	}
+	// #2. parse tail nested again
+	return parseIdentifierTail(p, vNode)
+}
+
+// callback -
+func cbIdentifier(vNode *VarAssignNode) func(tk *lex.Token) {
+	return func(tk *lex.Token) {
+		// append variables
+		vNode.Variables = append(vNode.Variables, &IdentifierNode{
+			literal: string(tk.Literal),
+		})
+	}
+}
