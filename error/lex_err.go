9954340405ac259c3e5554d0f46eadc54720e4ad
DemoHn <demohn2016@gmail.com>
2020-01-16 01:38:29 +0800

    feat: add formal errors for lexer

--- error/lex_err.go
+++ error/lex_err.go
@@ -5,11 +5,16 @@ import (
 )
 
 // InvalidSingleEllipsis -
-// TODO: add cursor
-func InvalidSingleEllipsis(idx int) *Error {
+func InvalidSingleEllipsis() *Error {
+	return lexError.NewError(0x01, Error{
+		text: "未能识别单个「…」字符，或许应该是「……」？",
+	})
+}
+
+// InvalidSingleEqual -
+func InvalidSingleEqual() *Error {
 	return lexError.NewError(0x02, Error{
-		text: "未能识别单个「…」字符，请注意需要有连续两个方可有效！",
-		info: idx,
+		text: "未能识别单个「=」字符，或许应该是「==」？",
 	})
 }
 
@@ -46,5 +51,37 @@ func InvalidIndentSpaceCount(count int) *Error {
 	return lexError.NewError(0x22, Error{
 		text: fmt.Sprintf("当缩进类型为「空格」，其所列字符数应为4之倍数：当前空格字符数为%d", count),
 		info: count,
+	})
+}
+
+// QuoteStackFull -
+func QuoteStackFull(maxSize int) *Error {
+	return lexError.NewError(0x23, Error{
+		text: fmt.Sprintf("在文本中嵌套过多引号：最大可以嵌套%d层", maxSize),
+		info: maxSize,
+	})
+}
+
+// InvalidIdentifier -
+func InvalidIdentifier() *Error {
+	return lexError.NewError(0x24, Error{
+		text: "标识符不符合规范",
+		info: nil,
+	})
+}
+
+// IdentifierExceedLength -
+func IdentifierExceedLength(maxLen int32) *Error {
+	return lexError.NewError(0x25, Error{
+		text: fmt.Sprintf("标识符长度超过限制：最大可用长度为%d个字元", maxLen),
+		info: maxLen,
+	})
+}
+
+// InvalidChar -
+func InvalidChar(ch rune) *Error {
+	return lexError.NewError(0x26, Error{
+		text: fmt.Sprintf("未能识别字元「%c」", ch),
+		info: ch,
 	})
 }
