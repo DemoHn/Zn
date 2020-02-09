7378ab886f4f7640aca234d52d521db83672b07a
DemoHn <demohn2016@gmail.com>
2020-02-09 22:50:49 +0800

    feat: refactor symbol table to support nest scope

--- exec/symbol.go
+++ exec/symbol.go
@@ -1,34 +1,124 @@
 package exec
 
-// SymbolTable - a global symbol table stores all variables
+import (
+	"fmt"
+	"strings"
+
+	"github.com/DemoHn/Zn/error"
+)
+
+// SymbolInfo - symbol info
+type SymbolInfo struct {
+	nestLevel int
+	value     ZnObject
+}
+
+// SymbolTable - a global hash-table manages all symbols
+// including variable, func name, etcs.
+// (support nesting)
 type SymbolTable struct {
-	Symbols map[string]ZnObject
+	// key   -> symbol name
+	// value -> symbol info
+	symbolMap map[string][]SymbolInfo
+	nestLevel int
 }
 
-// Insert - insert one symbol into symbolTable
-// if ID has exists, return false;
-// if not, return true;
-func (st *SymbolTable) Insert(id string, obj ZnObject) bool {
-	if _, ok := st.Symbols[id]; !ok {
-		st.Symbols[id] = obj
-		return true
+// NewSymbolTable -
+func NewSymbolTable() *SymbolTable {
+	return &SymbolTable{
+		symbolMap: map[string][]SymbolInfo{},
+		nestLevel: 0,
 	}
-	return false
 }
 
-// Lookup - find the value from symbol table
-func (st *SymbolTable) Lookup(id string) (ZnObject, bool) {
-	if val, ok := st.Symbols[id]; ok {
-		return val, true
+// Bind - add value to symbol table
+func (st *SymbolTable) Bind(id string, obj ZnObject) *error.Error {
+	newInfo := SymbolInfo{
+		nestLevel: st.nestLevel,
+		value:     obj,
 	}
-	return nil, false
+
+	symArr, ok := st.symbolMap[id]
+	if !ok {
+		// init symbolInfo array
+		st.symbolMap[id] = []SymbolInfo{newInfo}
+		return nil
+	}
+
+	// check if there's variable re-declaration
+	if len(symArr) > 0 && symArr[0].nestLevel == st.nestLevel {
+		return error.NewErrorSLOT("variable re-declaration")
+	}
+
+	// prepend data
+	st.symbolMap[id] = append([]SymbolInfo{newInfo}, st.symbolMap[id]...)
+	return nil
 }
 
-// SetData - set symbol table data
-func (st *SymbolTable) SetData(id string, obj ZnObject) bool {
-	if _, ok := st.Symbols[id]; ok {
-		st.Symbols[id] = obj
-		return true
+// Lookup - find the corresponded value from ID,
+// if nothing found, return error
+func (st *SymbolTable) Lookup(id string) (ZnObject, *error.Error) {
+	symArr, ok := st.symbolMap[id]
+	if !ok {
+		return nil, error.NewErrorSLOT("no valid variable found")
 	}
-	return false
+
+	// find the nearest level of value
+	if symArr == nil || len(symArr) == 0 {
+		return nil, error.NewErrorSLOT("no valid variable found")
+	}
+	return symArr[0].value, nil
+}
+
+// EnterScope - enter a nested scope
+func (st *SymbolTable) EnterScope() {
+	st.nestLevel++
+}
+
+// ExitScope - exit from a nested scope
+func (st *SymbolTable) ExitScope() {
+	// find all variable
+	for idx, symArr := range st.symbolMap {
+		if symArr != nil && len(symArr) > 0 {
+			if symArr[0].nestLevel == st.nestLevel {
+				// remove first item since it's outdated
+				st.symbolMap[idx] = symArr[1:]
+			}
+		}
+	}
+
+	st.nestLevel--
+}
+
+// SetData -
+func (st *SymbolTable) SetData(id string, obj ZnObject) *error.Error {
+	symArr, ok := st.symbolMap[id]
+	if !ok {
+		return error.NewErrorSLOT("variable not defined!")
+	}
+
+	if symArr != nil && len(symArr) > 0 {
+		symArr[0].value = obj
+		return nil
+	}
+
+	return error.NewErrorSLOT("variable not defined!")
+}
+
+func (st *SymbolTable) printSymbols() string {
+	strs := []string{}
+	for k, symArr := range st.symbolMap {
+		if symArr != nil {
+			for _, symItem := range symArr {
+				symStr := "ε"
+				if symItem.value != nil {
+					symStr = symItem.value.String()
+				}
+				strs = append(strs, fmt.Sprintf("‹%s, %d› => %s", k, symItem.nestLevel, symStr))
+			}
+		}
+
+	}
+
+	return strings.Join(strs, "\n")
 }
