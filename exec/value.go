9894ce01bc3d9bc495aa816cb58dbe6622c9818c
DemoHn <demohn2016@gmail.com>
2020-02-15 17:55:47 +0800

    feat: finish draft of rev.01

--- exec/value.go
+++ exec/value.go
@@ -2,9 +2,11 @@ package exec
 
 import (
 	"fmt"
+	"math/big"
 	"strings"
 
 	"github.com/DemoHn/Zn/error"
+	"github.com/DemoHn/Zn/syntax"
 )
 
 // ZnValue - general value interface
@@ -30,7 +32,7 @@ type ZnString struct {
 }
 
 func (zs *ZnString) String() string {
-	return fmt.Sprintf("「%s」", zs.Value)
+	return fmt.Sprintf("%s", zs.Value)
 }
 
 // NewZnString -
@@ -280,14 +282,107 @@ func NewZnNull() *ZnNull {
 	return &ZnNull{}
 }
 
+type funcExecutor func(params []ZnValue, execBlock *syntax.BlockStmt, st *SymbolTable) (ZnValue, *error.Error)
+
+// ZnFunction -
+type ZnFunction struct {
+	FuncName  string
+	ExecBlock *syntax.BlockStmt
+	Executor  funcExecutor
+}
+
+func (zf *ZnFunction) String() string {
+	return fmt.Sprintf("‹方法 %s›", zf.FuncName)
+}
+
+// Exec - exec function
+func (zf *ZnFunction) Exec(params []ZnValue, st *SymbolTable) (ZnValue, *error.Error) {
+	// st -> global symbol table
+	return zf.Executor(params, zf.ExecBlock, st)
+}
+
+// NewZnFunction -
+func NewZnFunction(funcName string, execBlock *syntax.BlockStmt, executor funcExecutor) *ZnFunction {
+	return &ZnFunction{
+		FuncName:  funcName,
+		ExecBlock: execBlock,
+		Executor:  executor,
+	}
+}
+
+// （显示） 方法的执行逻辑
+var displayExecutor = func(params []ZnValue, execBlock *syntax.BlockStmt, st *SymbolTable) (ZnValue, *error.Error) {
+	// display format string
+	var items = []string{}
+
+	for _, param := range params {
+		items = append(items, param.String())
+	}
+	fmt.Printf("%s\n", strings.Join(items, " "))
+	return NewZnNull(), nil
+}
+
+// （递增）方法的执行逻辑
+var addValueExecutor = func(params []ZnValue, execBlock *syntax.BlockStmt, st *SymbolTable) (ZnValue, *error.Error) {
+	var decimals = []*ZnDecimal{}
+	// validate types
+	for _, param := range params {
+		vparam, ok := param.(*ZnDecimal)
+		if !ok {
+			return nil, error.NewErrorSLOT("入参皆须为「数值」类型")
+		}
+		decimals = append(decimals, vparam)
+	}
+
+	sum, _ := NewZnDecimal("0")
+	for _, decimal := range decimals {
+		r1, r2 := rescalePair(sum, decimal)
+		newco := new(big.Int).Add(r1.co, r2.co)
+
+		sum.co = newco
+		sum.exp = r1.exp
+	}
+
+	return sum, nil
+}
+
+// （递减）方法的执行逻辑
+var subValueExecutor = func(params []ZnValue, execBlock *syntax.BlockStmt, st *SymbolTable) (ZnValue, *error.Error) {
+	var decimals = []*ZnDecimal{}
+	// validate types
+	for _, param := range params {
+		vparam, ok := param.(*ZnDecimal)
+		if !ok {
+			return nil, error.NewErrorSLOT("入参皆须为「数值」类型")
+		}
+		decimals = append(decimals, vparam)
+	}
+
+	sum, _ := NewZnDecimal("0")
+	for _, decimal := range decimals {
+		r1, r2 := rescalePair(sum, decimal)
+		negco := new(big.Int).Neg(r2.co)
+		newco := new(big.Int).Add(r1.co, negco)
+
+		sum.co = newco
+		sum.exp = r1.exp
+	}
+
+	return sum, nil
+}
+
 // init function
 func init() {
 	//// predefined values - those variables (symbols) are defined before
 	//// any execution procedure.
 	//// NOTICE: those variables are all constants!
 	predefinedValues = map[string]ZnValue{
-		"真": NewZnBool(true),
-		"假": NewZnBool(false),
-		"空": NewZnNull(),
+		"真":  NewZnBool(true),
+		"假":  NewZnBool(false),
+		"空":  NewZnNull(),
+		"显示": NewZnFunction("显示", nil, displayExecutor),
+		"递增": NewZnFunction("递增", nil, addValueExecutor),
+		"递加": NewZnFunction("递增", nil, addValueExecutor),
+		"递减": NewZnFunction("递减", nil, subValueExecutor),
 	}
 }
