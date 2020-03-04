c0467a92e8412965092452b752ecd61ea8e8e21a
DemoHn <demohn2016@gmail.com>
2020-03-05 01:54:45 +0800

    feat: add meetStmtLineBreak() to handle line-breaks

--- syntax/parser.go
+++ syntax/parser.go
@@ -8,8 +8,7 @@ import (
 // Parser - parse all nodes
 type Parser struct {
 	*lex.Lexer
-	tokens   [3]*lex.Token
-	lineMask uint16
+	tokens [3]*lex.Token
 }
 
 const (
@@ -59,15 +58,6 @@ func (p *Parser) next() *lex.Token {
 		panic(err)
 	}
 
-	// after retrieving next token successfully, check if current token has
-	// violate lineMasks
-	// check the comment of validateLineMask() for details
-	if p.tokens[0] != nil && p.tokens[1] != nil {
-		if err = p.validateLineMask(p.tokens[0], p.tokens[1]); err != nil {
-			panic(err)
-		}
-	}
-
 	// move advanced token buffer
 	p.tokens[0] = p.tokens[1]
 	p.tokens[1] = p.tokens[2]
@@ -88,31 +78,103 @@ func (p *Parser) peek2() *lex.Token {
 	return p.tokens[2]
 }
 
-func (p *Parser) setLineMask(loc uint16) {
-	p.lineMask = p.lineMask | loc
-}
+// meetStmtLineBreak - if there's a statement line-break at the end of token.
+//
+// StmtLineBreak is not an existing token, i.e. it wouldn't insert into token stream.
+// It's a virtual mark that separates parsing statements, thus you can image it like a
+// "virtually" inserted semicolon, like the following code:
+//
+// 如果此IDE之 ';' <-- here is the statement line-break
+//     名为「VSCODE」
+//
+// There's stmt line-break at the end of first line, thus the process of parsing IF-statement
+// should terminate due to lacking matched tokens, similiar to the behaviour that an semicolon is inserted.
+//
+// Theoratically, any type of token that located at the end of line
+//
+//   i.e.  this token is the last token of current line,
+//   or    $token.Range.EndLine < ($token+1).Range.StartLine,
+//
+// should meet StmtLineBreak, which means meetStmtLineBreak() = true. Still, there're some
+// exceptions listed below:
+//
+//   1.    token type is one of the following 5 punctuations:  ，  {  【  ：  ？
+//   or
+//   2.    the next token type if one of the following 3 marks:  】  }  EOF
+//
+// Example 1#, 2#, 3# illustrates those exceptions that even if there're two or more lines, it's still
+// a valid and complete statement:
+//
+// Example 1#
+//
+// （显示并连缀：
+//     「结果为」，
+//      人口-中位数）
+//
+// Example 2#
+//
+// 令天干地支表为【
+//     「子」 == 「甲」，
+//     「丑」 == 「乙」，
+//     「寅」 == 「丙」，
+//     「卯」 == 「丁」
+// 】
+//
+// Example 3#
+//
+// ·时·等于12 且{
+//     ·分·等于0 或 ·分·等于30
+// }等于真
+func (p *Parser) meetStmtLineBreak() bool {
+	current := p.current()
+	peek := p.peek()
 
-func (p *Parser) unsetLineMask(loc uint16) {
-	p.lineMask = p.lineMask & (^loc)
-}
-
-func (p *Parser) validateLineMask(lastToken *lex.Token, newToken *lex.Token) *error.Error {
-
-	var line1 = lastToken.Range.EndLine
-	var line2 = newToken.Range.StartLine
-	// if new token has entered a new line
-	if line2 > line1 {
-		// for modeInline, all tokens should have no explicit newline (CRLF)
-		if (p.lineMask & modeInline) > 0 {
-			return error.IncompleteStmt()
-		}
+	exceptCurrentTokenTypes := []lex.TokenType{
+		lex.TypeCommaSep,
+		lex.TypeStmtQuoteL,
+		lex.TypeArrayQuoteL,
+		lex.TypeFuncCall,
+		lex.TypeFuncDeclare,
 	}
-	return nil
+
+	exceptFollowingTokenTypes := []lex.TokenType{
+		lex.TypeArrayQuoteR,
+		lex.TypeStmtQuoteR,
+	}
+
+	if peek == nil || current == nil {
+		return false
+	}
+	if current.Type == lex.TypeEOF || peek.Type == lex.TypeEOF {
+		return false
+	}
+
+	// current token is at line end
+	if peek.Range.StartLine > current.Range.EndLine {
+		// exception rule 1
+		for _, currTk := range exceptCurrentTokenTypes {
+			if currTk == current.Type {
+				return false
+			}
+		}
+		// exception rule 2
+		for _, followingTk := range exceptFollowingTokenTypes {
+			if followingTk == peek.Type {
+				return false
+			}
+		}
+		return true
+	}
+	return false
 }
 
 // consume one token with denoted validTypes
 // if not, return syntaxError
 func (p *Parser) consume(validTypes ...lex.TokenType) {
+	if p.meetStmtLineBreak() {
+		panic(error.InvalidSyntaxCurr())
+	}
+
 	tkType := p.peek().Type
 	for _, item := range validTypes {
 		if item == tkType {
@@ -129,6 +191,10 @@ func (p *Parser) consume(validTypes ...lex.TokenType) {
 //
 // returns (matched, tokenType)
 func (p *Parser) tryConsume(validTypes ...lex.TokenType) (bool, *lex.Token) {
+	if p.meetStmtLineBreak() {
+		return false, nil
+	}
+
 	tk := p.peek()
 	for _, vt := range validTypes {
 		if vt == tk.Type {
@@ -162,6 +228,8 @@ func (p *Parser) getPeekIndent() int {
 	return p.GetLineIndent(peekLine)
 }
 
+//// helper functions
+
 // similar to lexer's version, but with given line & col
 func moveAndSetCursor(p *Parser, line int, col int, err *error.Error) {
 	buf := p.GetLineBuffer()
@@ -179,7 +247,11 @@ func moveAndSetCursor(p *Parser, line int, col int, err *error.Error) {
 	}()
 
 	endCursor := p.SlideToLineEnd()
-	cursor.Text = string(buf[:endCursor+1])
+	if line < p.CurrentLine {
+		cursor.Text = string(p.GetParsedLineText(line))
+	} else {
+		cursor.Text = string(buf[:endCursor+1])
+	}
 	err.SetCursor(cursor)
 }
 
