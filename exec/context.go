48eedca7e83820dd124b17ab3d67037ce6699f9e
DemoHn <demohn2016@gmail.com>
2020-06-21 15:15:10 +0800

    feat: move

--- exec/context.go
+++ exec/context.go
@@ -53,64 +53,6 @@ func NewContext() *Context {
 	}
 }
 
-// BindSymbol - add value to symbol table
-func (ctx *Context) BindSymbol(nestLevel int, id string, obj ZnValue, isConstant bool) *error.Error {
-	newInfo := SymbolInfo{
-		NestLevel:  nestLevel,
-		Value:      obj,
-		IsConstant: isConstant,
-	}
-
-	symArr, ok := ctx.symbols[id]
-	if !ok {
-		// init symbolInfo array
-		ctx.symbols[id] = []SymbolInfo{newInfo}
-		return nil
-	}
-
-	// check if there's variable re-declaration
-	if len(symArr) > 0 && symArr[0].NestLevel == nestLevel {
-		return error.NameRedeclared(id)
-	}
-
-	// prepend data
-	ctx.symbols[id] = append([]SymbolInfo{newInfo}, ctx.symbols[id]...)
-	return nil
-}
-
-// LookupSymbol - find the corresponded value from ID,
-// if nothing found, return error
-func (ctx *Context) LookupSymbol(id string) (ZnValue, *error.Error) {
-	symArr, ok := ctx.symbols[id]
-	if !ok {
-		return nil, error.NameNotDefined(id)
-	}
-
-	// find the nearest level of value
-	if symArr == nil || len(symArr) == 0 {
-		return nil, error.NameNotDefined(id)
-	}
-	return symArr[0].Value, nil
-}
-
-// SetSymbol - after variable is defined, set the value from the label
-func (ctx *Context) SetSymbol(id string, obj ZnValue) *error.Error {
-	symArr, ok := ctx.symbols[id]
-	if !ok {
-		return error.NameNotDefined(id)
-	}
-
-	if symArr != nil && len(symArr) > 0 {
-		symArr[0].Value = obj
-		if symArr[0].IsConstant {
-			return error.AssignToConstant()
-		}
-		return nil
-	}
-
-	return error.NameNotDefined(id)
-}
-
 // GetSymbols -
 func (ctx *Context) GetSymbols() map[string][]SymbolInfo {
 	return ctx.symbols
