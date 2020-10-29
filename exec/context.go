2cb0c62d6fc1cec4ccdc51c651f73d975c37a771
DemoHn <demohn2016@gmail.com>
2020-10-29 22:37:18 +0800

    feat: start update

--- exec/context.go
+++ exec/context.go
@@ -2,79 +2,105 @@ package exec
 
 import (
 	"github.com/DemoHn/Zn/debug"
-	"github.com/DemoHn/Zn/error"
 	"github.com/DemoHn/Zn/lex"
-	"github.com/DemoHn/Zn/syntax"
 )
 
-// Context - GLOBAL execution context, usually create only once in one program.
+const arithPrecision = 8
+
+// Context is a global variable that stores current execution
+// states, global configurations
 type Context struct {
-	globals map[string]ZnValue
-	arith   *Arith
+	// globals - stores all global variables
+	globals map[string]Value
+	// arith - an arith object that manages all arith calculations
+	arith *Arith
+
 	// a seperate map to store inner debug data
 	// usage: call （__probe：「tagName」，variable）
 	// it will record all logs (including variable value, curernt scope, etc.)
 	// the value is deep-copied so don't worry - the value logged won't be changed
 	_probe *debug.Probe
+	scope  *Scope
 }
 
-const defaultPrecision = 8
-
-// Result - context execution result structure
-// NOTICE: when HasError = true, Value = nil, while execution yields error
-//         when HasError = false, Error = nil, Value = <result Value>
-//
-// Currently only one value is supported as return argument.
-type Result struct {
-	HasError bool
-	Value    ZnValue
-	Error    *error.Error
+// Scope represents a local namespace (aka. environment) of current execution
+// including local variables map and current "return" value.
+type Scope struct {
+	// fileInfo -
+	fileInfo *FileInfo
+	// classRefMap stores class definition template (reference) within the scope
+	// this item only exists on RootScope since class defition block IS allowed
+	// ONLY in root block
+	classRefMap map[string]*ClassRef
+	// parent - parent Scope
+	parent      *Scope
+	symbolMap   map[string]SymbolInfo
+	returnValue Value
 }
 
-// NewContext - create new Zn Context for furthur execution
+// FileInfo records current file info, usually for displaying error
+type FileInfo struct {
+	//// lexical scope
+	// file - current execution file directory
+	file string
+	// currentLine - current exeuction line
+	currentLine int
+	// lineStack - lexical info of (parsed) current file
+	lineStack *lex.LineStack
+}
+
+// SymbolInfo - a wrapper of symbol's value with additional properties.
+type SymbolInfo struct {
+	value Value
+	// Constant - if an symbol is const
+	isConst bool
+}
+
+// NewContext - create new Zn Context. Notice through the life-cycle
+// of one code execution, there's only one running context to store all states.
 func NewContext() *Context {
 	return &Context{
-		globals: predefinedValues,
+		globals: map[string]Value{}, // TODO
 		arith:   NewArith(defaultPrecision),
 		_probe:  debug.NewProbe(),
+		scope:   nil,
 	}
 }
 
-// ExecuteCode - execute program from input Zn code (whether from file or REPL)
-func (ctx *Context) ExecuteCode(in *lex.InputStream, scope *RootScope) Result {
-	l := lex.NewLexer(in)
-	p := syntax.NewParser(l)
-	// start
-	block, err := p.Parse()
-	if err != nil {
-		return Result{true, nil, err}
+// InitScope - init root scope
+func (ctx *Context) InitScope(l *lex.Lexer) {
+	fileInfo := &FileInfo{
+		file:        l.InputStream.GetFile(),
+		currentLine: 0,
+		lineStack:   l.LineStack,
 	}
-	// init scope
-	scope.Init(l)
-
-	// construct root (program) node
-	program := syntax.NewProgramNode(block)
-
-	// eval program
-	if err := evalProgram(ctx, scope, program); err != nil {
-		wrapError(ctx, scope, err)
-		return Result{true, nil, err}
+	newScope := &Scope{
+		fileInfo:    fileInfo,
+		classRefMap: map[string]*ClassRef{},
+		parent:      nil,
+		symbolMap:   map[string]SymbolInfo{},
+		returnValue: nil,
 	}
-	return Result{false, scope.GetLastValue(), nil}
+	ctx.scope = newScope
 }
 
-// wrapError if lineInfo is missing (mostly for non-syntax errors)
-// If lineInfo missing, then we will add current execution line and hide some part to
-// display errors properly.
-func wrapError(ctx *Context, scope *RootScope, err *error.Error) {
-	cursor := err.GetCursor()
+// DuplicateNewScope - create a new Context with new scope which parent points to duplicator's scope.
+func (ctx *Context) DuplicateNewScope() *Context {
+	newContext := *ctx
+	newContext.scope = createChildScope(ctx.scope)
 
-	if cursor.LineNum == 0 {
-		newCursor := error.Cursor{
-			File:    scope.file,
-			LineNum: scope.currentLine,
-			Text:    scope.lineStack.GetLineText(scope.currentLine, false),
-		}
-		err.SetCursor(newCursor)
+	return &newContext
+}
+
+//// helpers
+func createChildScope(old *Scope) *Scope {
+	newScope := &Scope{
+		fileInfo:    old.fileInfo,
+		classRefMap: old.classRefMap,
+		parent:      old,
+		symbolMap:   map[string]SymbolInfo{},
+		returnValue: nil,
 	}
+
+	return newScope
 }
---
+++ exec/value.go
@@ -0,0 +1,225 @@
+package exec
+
+import (
+	"github.com/DemoHn/Zn/error"
+	"github.com/DemoHn/Zn/syntax"
+)
+
+type funcExecutor func(*Context, []Value) (Value, *error.Error)
+
+// Value is the base unit to present a value (aka. variable) - including number, string, array, function, object...
+// All kinds of values in Zn language SHOULD implement this interface.
+//
+// Basically there're 3 methods:
+//
+// 1. GetProperty - fetch the value from property list of a specific name
+// 2. SetProperty - set the value of some property
+// 3. ExecMethod - execute one method from method list
+type Value interface {
+	GetProperty(*Context, string) (Value, *error.Error)
+	SetProperty(*Context, string, Value) *error.Error
+	ExecMethod(*Context, string, []Value) (Value, *error.Error)
+}
+
+// ClosureRef - aka. Closure Exection Reference
+// It's the structure of a closure which wraps execution logic.
+// The executor could be either a bunch of code or some native code.
+type ClosureRef struct {
+	ParamHandler funcExecutor
+	Executor     funcExecutor // closure execution logic
+}
+
+// BuildClosureFromNode - create a closure (with default param handler logic)
+// from Zn code (*syntax.BlockStmt). It's the constructor of 如何XX or (anoymous function in the future)
+func BuildClosureFromNode(paramTags []*syntax.ParamItem, stmtBlock *syntax.BlockStmt) ClosureRef {
+	var executor = func(ctx *Context, params []Value) (Value, *error.Error) {
+		// iterate block round I - function hoisting
+		// NOTE: function hoisting means bind function definitions at the beginning
+		// of execution so that even if "function execution" statement is before
+		// "function definition" statement.
+		for _, stmtI := range stmtBlock.Children {
+			if v, ok := stmtI.(*syntax.FunctionDeclareStmt); ok {
+				fn := BuildZnFunctionFromNode(v)
+				if err := bindValue(ctx, v.FuncName.GetLiteral(), fn); err != nil {
+					return nil, err
+				}
+			}
+		}
+		// iterate block round II - execution of rest code blocks
+		for _, stmtII := range stmtBlock.Children {
+			if _, ok := stmtII.(*syntax.FunctionDeclareStmt); !ok {
+				if err := evalStatement(ctx, stmtII); err != nil {
+					// if recv breaks
+					if err.GetCode() == error.ReturnBreakSignal {
+						if extra, ok := err.GetExtra().(Value); ok {
+							return extra, nil
+						}
+					}
+					return nil, err
+				}
+			}
+		}
+		return scope.GetReturnValue(), nil
+	}
+
+	var paramHandler = func(ctx *Context, params []Value) (Value, *error.Error) {
+		// check param length
+		if len(params) != len(paramTags) {
+			return nil, error.MismatchParamLengthError(len(paramTags), len(params))
+		}
+
+		// bind params (as variable) to function scope
+		for idx, paramVal := range params {
+			param := paramTags[idx]
+			// if param is NOT a reference type, then we need additionally
+			// copy its value
+			if !param.RefMark {
+				paramVal = duplicateValue(paramVal)
+			}
+			paramName := param.ID.GetLiteral()
+			if err := bindValue(ctx, paramName, paramVal); err != nil {
+				return nil, err
+			}
+		}
+		return nil, nil
+	}
+
+	return ClosureRef{
+		ParamHandler: paramHandler,
+		Executor:     executor,
+	}
+}
+
+// NewClosure - wraps a closure from native code (Golang code)
+func NewClosure(paramHandler funcExecutor, executor funcExecutor) ClosureRef {
+	return ClosureRef{
+		ParamHandler: paramHandler,
+		Executor:     executor,
+	}
+}
+
+// Exec - execute a closure - accepts input params, execute from closure exeuctor and
+// yields final result
+func (cs *ClosureRef) Exec(ctx *Context, params []Value) (Value, *error.Error) {
+	if cs.ParamHandler != nil {
+		if _, err := cs.ParamHandler(ctx, params); err != nil {
+			return nil, err
+		}
+	}
+	if cs.Executor == nil {
+		return nil, error.NewErrorSLOT("执行逻辑不能为空")
+	}
+	// do execution
+	return cs.Executor(ctx, params)
+}
+
+// ClassRef - aka. Class Definition Reference
+// It defines the structure of a class, including compPropList, methodList and propList.
+// All instances created from this class MUST inherits from those configurations.
+type ClassRef struct {
+	// Name - class name
+	Name string
+	// Constructor defines default logic (mostly for initialization) when a new instance
+	// is created by "x 成为 C：P，Q，R"
+	Constructor funcExecutor
+	// PropList defines all property name of a class, each item COULD NOT BE neither append nor removed
+	PropList []string
+	// CompPropList - CompProp stands for "Computed Property", which means the value is get or set
+	// from a pre-defined function. Computed property offers more extensions for manipulations
+	// of properties.
+	CompPropList map[string]ClosureRef
+	// MethodList - stores all available methods defintion of class
+	MethodList map[string]ClosureRef
+}
+
+// BuildClassFromNode -
+func BuildClassFromNode(name string, classNode *syntax.ClassDeclareStmt) ClassRef {
+	ref := ClassRef{
+		Name:         name,
+		Constructor:  nil,
+		PropList:     []string{},
+		CompPropList: map[string]ClosureRef{},
+		MethodList:   map[string]ClosureRef{},
+	}
+
+	// define default constrcutor
+	var constructor = func(ctx *Context, params []Value) (Value, *error.Error) {
+		obj := NewZnObject(ref)
+		// init prop list
+		for _, propPair := range classNode.PropertyList {
+			propID := propPair.PropertyID.GetLiteral()
+			expr, err := evalExpression(ctx, propPair.InitValue)
+			if err != nil {
+				return nil, err
+			}
+			obj.PropList[propID] = expr
+			ref.PropList = append(ref.PropList, propID)
+		}
+		// constructor: set some properties' value
+		if len(params) != len(classNode.ConstructorIDList) {
+			return nil, error.MismatchParamLengthError(len(params), len(classNode.ConstructorIDList))
+		}
+		for idx, objParamVal := range params {
+			param := classNode.ConstructorIDList[idx]
+			// if param is NOT a reference, then we need to copy its value
+			if !param.RefMark {
+				objParamVal = duplicateValue(objParamVal)
+			}
+			paramName := param.ID.GetLiteral()
+			obj.PropList[paramName] = objParamVal
+		}
+
+		return obj, nil
+	}
+	// set constructor
+	ref.Constructor = constructor
+
+	// add getters
+	for _, gNode := range classNode.GetterList {
+		getterTag := gNode.GetterName.GetLiteral()
+		ref.CompPropList[getterTag] = BuildClosureFromNode([]*syntax.ParamItem{}, gNode.ExecBlock)
+	}
+
+	// add methods
+	for _, mNode := range classNode.MethodList {
+		mTag := mNode.FuncName.GetLiteral()
+		ref.MethodList[mTag] = BuildClosureFromNode(mNode.ParamList, mNode.ExecBlock)
+	}
+
+	return ref
+}
+
+// NewClassRef - create new empty ClassRef
+func NewClassRef(name string) ClassRef {
+	return ClassRef{
+		Name:         name,
+		Constructor:  nil,
+		PropList:     []string{},
+		CompPropList: map[string]ClosureRef{},
+		MethodList:   map[string]ClosureRef{},
+	}
+}
+
+// Construct - yield new instance of this class
+func (cr *ClassRef) Construct(ctx *Context, params []Value) (Value, *error.Error) {
+	return cr.Constructor(ctx, params)
+}
+
+// Null type - "null" is a special value that represents for "nothing".
+// a null value only "equals to" null. (XX 为 空)
+type Null struct{}
+
+// GetProperty - a null Value does not have ANY propreties.
+func (nl *Null) GetProperty(ctx *Context, name string) (Value, *error.Error) {
+	return nil, error.PropertyNotFound(name)
+}
+
+// SetProperty - a null Value does not have ANY propreties.
+func (nl *Null) SetProperty(ctx *Context, name string, value Value) (Value, *error.Error) {
+	return nil, error.PropertyNotFound(name)
+}
+
+// ExecMethod - a null value does not have ANY methods.
+func (nl *Null) ExecMethod(ctx *Context, name string, values []Value) (Value, *error.Error) {
+	return nil, error.MethodNotFound(name)
+}
