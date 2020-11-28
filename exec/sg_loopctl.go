10fc5d1a7db2a21df7acad7f61c8d4cc32731ef5
DemoHn <demohn2016@gmail.com>
2020-11-28 12:19:16 +0800

    feat: implement findSgValue() logic

---
+++ exec/sg_loopctl.go
@@ -0,0 +1,51 @@
+package exec
+
+import "github.com/DemoHn/Zn/error"
+
+// LoopCtl - a sgValue type used to control loop flow (aka. continue/break)
+// and record values of current iteration.
+type LoopCtl struct {
+	currentIndex Value
+	currentValue Value
+}
+
+// NewLoopCtl -
+func NewLoopCtl() *LoopCtl {
+	return &LoopCtl{
+		currentIndex: NewNull(),
+		currentValue: NewNull(),
+	}
+}
+
+// GetProperty -
+func (lc *LoopCtl) GetProperty(ctx *Context, name string) (Value, *error.Error) {
+	switch name {
+	case "值":
+		return lc.currentValue, nil
+	case "索引":
+		return lc.currentIndex, nil
+	}
+	return nil, error.PropertyNotFound(name)
+}
+
+// SetProperty -
+func (lc *LoopCtl) SetProperty(ctx *Context, name string, value Value) *error.Error {
+	return error.PropertyNotFound(name)
+}
+
+// ExecMethod -
+func (lc *LoopCtl) ExecMethod(ctx *Context, name string, values []Value) (Value, *error.Error) {
+	switch name {
+	case "结束":
+		return NewNull(), error.BreakBreakError()
+	case "继续":
+		return NewNull(), error.ContinueBreakError()
+	}
+	return nil, error.MethodNotFound(name)
+}
+
+// SetCurrentValue - internal usage to set current value
+func (lc *LoopCtl) SetCurrentValue(index Value, value Value) {
+	lc.currentIndex = index
+	lc.currentValue = value
+}
