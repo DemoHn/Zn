be3f6dc4f2f368f94132547624213bd6436450fb
DemoHn <demohn2016@gmail.com>
2020-12-28 23:47:28 +0800

    feat: add doc and impl methods

--- exec/v_array.go
+++ exec/v_array.go
@@ -25,26 +25,54 @@ func (ar *Array) GetProperty(ctx *Context, name string) (Value, *error.Error) {
 		return mulValueExecutor(ctx, ar.value)
 	case "商":
 		return divValueExecutor(ctx, ar.value)
-	case "首":
+	case "首", "首项", "第一项":
 		if len(ar.value) == 0 {
 			return NewNull(), nil
 		}
 		return ar.value[0], nil
-	case "尾":
+	case "尾", "末项", "最后项":
 		if len(ar.value) == 0 {
 			return NewNull(), nil
 		}
 		return ar.value[len(ar.value)-1], nil
-
 	case "数目", "长度":
 		l := len(ar.value)
 		return NewDecimalFromInt(l, 0), nil
+	case "文本*":
+		valStr := StringifyValue(ar)
+		return NewString(valStr), nil
+	case "逆":
+		result := []Value{}
+		l := len(ar.value)
+		for i := 0; i < l; i++ {
+			result = append(result, result[l-1-i])
+		}
+
+		return NewArray(result), nil
 	}
 	return nil, error.PropertyNotFound(name)
 }
 
 // SetProperty -
 func (ar *Array) SetProperty(ctx *Context, name string, value Value) *error.Error {
+	switch name {
+	case "首", "首项", "第一项":
+		if len(ar.value) == 0 {
+			result := []Value{value}
+			ar.value = result
+			return nil
+		}
+		ar.value[0] = value
+		return nil
+	case "尾", "末项", "最后项":
+		if len(ar.value) == 0 {
+			result := []Value{value}
+			ar.value = result
+			return nil
+		}
+		ar.value[len(ar.value)-1] = value
+		return nil
+	}
 	return error.PropertyNotFound(name)
 }
 
