c5d8c455c13c6e9a2fcfadde0a477978abb77142
DemoHn <demohn2016@gmail.com>
2021-06-05 11:04:20 +0800

    feat: add generate JSON logic

--- exec/stdlib/lib_json.go
+++ exec/stdlib/lib_json.go
@@ -11,8 +11,10 @@ import (
 
 var libjsonMethodMap = map[string]ctx.Value{
 	"解析JSON": val.NewFunction("解析JSON", parseJsonFunc),
+	"生成JSON": val.NewFunction("生成JSON", generateJsonFunc),
 }
 
+// parseJsonFunc - 解析JSON
 func parseJsonFunc(c *ctx.Context, values []ctx.Value) (ctx.Value, *error.Error) {
 	// validate string ONLY
 	if err := val.ValidateExactParams(values, "string"); err != nil {
@@ -27,43 +29,77 @@ func parseJsonFunc(c *ctx.Context, values []ctx.Value) (ctx.Value, *error.Error)
 		return nil, error.NewErrorSLOT("解析JSON失败：" + err.Error())
 	}
 
-	return buildHashMap(result), nil
+	return buildHashMapItem(result), nil
 }
 
-// buildHashMap - build hash map
-func buildHashMap(source map[string]interface{}) *val.HashMap {
-	target := val.NewHashMap([]val.KVPair{})
-	for k, v := range source {
-		finalValue := buildHashMapItem(v)
-		target.AppendKVPair(val.KVPair{
-			Key:   k,
-			Value: finalValue,
-		})
+// generateJsonFunc - 生成JSON
+func generateJsonFunc(c *ctx.Context, values []ctx.Value) (ctx.Value, *error.Error) {
+	if err := val.ValidateExactParams(values, "hashmap"); err != nil {
+		return nil, err
 	}
-	return target
+	data, err := json.Marshal(buildPlainStrItem(values[0]))
+	if err != nil {
+		return nil, error.NewErrorSLOT("生成JSON失败：" + err.Error())
+	}
+	return val.NewString(string(data)), nil
 }
 
 // buildHashMapItem -
 func buildHashMapItem(item interface{}) ctx.Value {
-	var finalValue ctx.Value
+	if item == nil { // nil for json value "null"
+		return val.NewNull()
+	}
 	switch vv := item.(type) {
 	case float64:
-		finalValue = val.NewDecimalFromInt(int(vv), 0)
+		return val.NewDecimalFromInt(int(vv), 0)
 	case string:
-		finalValue = val.NewString(vv)
+		return val.NewString(vv)
 	case bool:
-		finalValue = val.NewBool(vv)
+		return val.NewBool(vv)
 	case map[string]interface{}:
-		finalValue = buildHashMap(vv)
+		target := val.NewHashMap([]val.KVPair{})
+		for k, v := range vv {
+			finalValue := buildHashMapItem(v)
+			target.AppendKVPair(val.KVPair{
+				Key:   k,
+				Value: finalValue,
+			})
+		}
+		return target
 	case []interface{}:
 		varr := val.NewArray([]ctx.Value{})
 		for _, vitem := range vv {
 			varr.AppendValue(buildHashMapItem(vitem))
 		}
-		finalValue = varr
+		return varr
 	default:
-		finalValue = val.NewString(fmt.Sprintf("%v", vv))
+		return val.NewString(fmt.Sprintf("%v", vv))
 	}
+}
 
-	return finalValue
+// buildPlainStrItem - from ctx.Value -> plain interface{} value
+func buildPlainStrItem(item ctx.Value) interface{} {
+	switch vv := item.(type) {
+	case *val.Null:
+		return nil
+	case *val.String:
+		return vv.String()
+	case *val.Bool:
+		return vv.GetValue()
+	case *val.Decimal:
+		return 0 // TODO: fix
+	case *val.Array:
+		resultList := []interface{}{}
+		for _, vi := range vv.GetValue() {
+			resultList = append(resultList, buildPlainStrItem(vi))
+		}
+		return resultList
+	case *val.HashMap:
+		resultMap := map[string]interface{}{}
+		for k, vi := range vv.GetValue() {
+			resultMap[k] = buildPlainStrItem(vi)
+		}
+		return resultMap
+	}
+	return nil
 }
