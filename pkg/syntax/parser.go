cd851b62be690cf5a611862469c5bd401a60b3e1
DemoHn <demohn2016@gmail.com>
2022-04-21 20:04:46 +0800

    feat: add astBuilder type

--- pkg/syntax/parser.go
+++ pkg/syntax/parser.go
@@ -3,7 +3,6 @@ package syntax
 // Parser - parse source file into syntax tree for further execution.
 type Parser struct {
 	*Lexer
-	TokenBuilder
 	// TokenP1: Peek1 token
 	TokenP1 Token
 	// TokenP2: Peek2 token
@@ -16,22 +15,44 @@ type TokenBuilder interface {
 	NextToken(lexer *Lexer) (Token, error)
 }
 
+// ASTBuilder - build AST from tokens. Its logic varies from different languages.
+// Currently, only Chinese ASTBuilder is supported
+type ASTBuilder interface {
+	ParseAST(parser *Parser) (*AST, error)
+}
+
 // NewParser - create a new parser from source
-func NewParser(lexer *Lexer, builder TokenBuilder) *Parser {
+func NewParser(lexer *Lexer) *Parser {
 	return &Parser{
 		Lexer:        lexer,
-		TokenBuilder: builder,
 	}
 }
 
 // Parser - parse all tokens into syntax tree
 // TODO: in the future we'll parse it into bytecodes directly, instead.
-func (p *Parser) Parse() (*AST, error) {
-	return nil, nil
+func (p *Parser) Parse(tkBuilder TokenBuilder, astBuilder ASTBuilder) (ast *AST, err error) {
+	// handle panics
+	defer func() {
+		var ok bool
+		if r := recover(); r != nil {
+			err, ok = r.(error)
+			if !ok {
+				panic(r)
+			}
+			// handleDeferError(p, err)
+		}
+	}()
+
+	// advance tokens TWICE
+	p.Next(tkBuilder)
+	p.Next(tkBuilder)
+
+	ast, err = astBuilder.ParseAST(p)
+	return
 }
 
-func (p *Parser) next() Token {
-	tk, err := p.NextToken(p.Lexer)
+func (p *Parser) Next(tkBuilder TokenBuilder) Token {
+	tk, err := tkBuilder.NextToken(p.Lexer)
 	if err != nil {
 		panic(err)
 	}
@@ -42,10 +63,10 @@ func (p *Parser) next() Token {
 	return p.TokenP1
 }
 
-func (p *Parser) current() Token {
+func (p *Parser) Current() Token {
 	return p.TokenP1
 }
 
-func (p *Parser) peek() Token {
+func (p *Parser) Peek() Token {
 	return p.TokenP2
 }
