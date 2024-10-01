191100eeb27882ebd82225ef410a3e5faaea962b
DemoHn <demohn2016@gmail.com>
2024-10-02 00:07:08 +0800

    feat: set name pipe func tpl for Windows

---
+++ pkg/server/name_pipe_windows.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"os"
+)
+
+const gPipeNameFmt = "/tmp/zinc-server-pipe-%s"
+
+type pipe struct {
+	id string
+}
+
+func NewPipe(id string) *pipe {
+	return &pipe{id: id}
+}
+
+func GetPipeID(p *pipe) string {
+	return p.id
+}
+
+func CreateNamedPipe() (*pipe, error) {
+	panic("Not Supported in Windows!")
+}
+
+func OpenNamedPipeReader(p *pipe) (*os.File, error) {
+	panic("Not Supported in Windows!")
+}
+
+func ReadDataFromNamedPipe(pipeReader *os.File, b []byte) error {
+	panic("Not Supported in Windows!")
+}
+
+func OpenNamedPipeWriter(p *pipe) (*os.File, error) {
+	panic("Not Supported in Windows!")
+}
+
+func WriteDataToNamedPipe(pipeWriter *os.File, b []byte) error {
+	panic("Not Supported in Windows!")
+}
