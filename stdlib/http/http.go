13e69dc18ee8815dd2fe3ea47c833a6a5c349e89
DemoHn <demohn2016@gmail.com>
2025-11-22 09:05:59 +0800

    feat: add send method

--- stdlib/http/http.go
+++ stdlib/http/http.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	libURL "net/url"
+
 	r "github.com/DemoHn/Zn/pkg/runtime"
 	"github.com/DemoHn/Zn/pkg/util"
 	"github.com/DemoHn/Zn/pkg/value"
@@ -49,6 +51,112 @@ var CLASS_HttpRequest = value.NewClassModel("HTTP请求").
 	DefineProperty("允许重定向", value.NewBool(true)).
 	DefineProperty("请求时限", value.NewNumber(30)).
 	DefineMethod("发送请求", value.NewFunction(methodSendRequest))
+
+func methodSendRequest(receiver r.Element, values []r.Element) (r.Element, error) {
+	receiver.GetProperty("路径")
+	path, err1 := value.AssertPropertyElement[*value.String](receiver, "路径")
+	method, err2 := value.AssertPropertyElement[*value.String](receiver, "方法")
+	headers, err3 := value.AssertPropertyElement[*value.HashMap](receiver, "头部")
+	// assert String or HashMap
+	queryParam, err4 := value.BuildEitherPropertyElement[*value.String, *value.HashMap](receiver, "查询参数")
+	// assert String or HashMap
+	body, err5 := value.BuildEitherPropertyElement[*value.String, *value.HashMap](receiver, "内容")
+	allowRedicrect, err6 := value.AssertPropertyElement[*value.Bool](receiver, "允许重定向")
+	timeout, err7 := value.AssertPropertyElement[*value.Number](receiver, "请求时限")
+
+	// assert errors
+	for _, e := range []error{err1, err2, err3, err4, err5, err6, err7} {
+		if e != nil {
+			return nil, e
+		}
+	}
+
+	// get params
+	pathValue := path.GetValue()
+	methodValue := method.GetValue()
+
+	// get headers
+	headersValue := make([][2]string, 0)
+	for k, v := range headers.GetValue() {
+		// only string Value is allowed to append into header set
+		// others will be filted without any warning wor~
+		if str, ok := v.(*value.String); ok {
+			headersValue = append(headersValue, [2]string{k, str.GetValue()})
+		}
+	}
+
+	// get query param
+	queryParamValue := make([][2]string, 0)
+	if queryParam.IsA() { // A:*String, B:*HashMap
+		qs := queryParam.GetA().GetValue()
+		qsValue, err := libURL.ParseQuery(qs)
+		if err != nil {
+			return nil, err
+		}
+		for k, v := range qsValue {
+			for _, item := range v {
+				queryParamValue = append(queryParamValue, [2]string{k, item})
+			}
+		}
+	} else {
+		// from HashMap -> map[string][]string
+		for k, v := range queryParam.GetB().GetValue() {
+			switch vv := v.(type) {
+			case *value.String:
+				queryParamValue = append(queryParamValue, [2]string{k, vv.GetValue()})
+			// handle Array of String
+			case *value.Array:
+				for _, inItem := range vv.GetValue() {
+					if inItemStr, ok := inItem.(*value.String); ok {
+						queryParamValue = append(queryParamValue, [2]string{k, inItemStr.GetValue()})
+					}
+				}
+			}
+		}
+	}
+
+	// convert reqBody
+	var reqBody util.ReqBody
+	if body.IsA() { // A:*String, B:*HashMap
+		reqBody.ContentType = "application/x-www-form-urlencoded"
+		reqBody.Value = body.GetA().GetValue()
+	} else {
+		bodyStr, err := util.HashMapToJSONString(body.GetB())
+		if err != nil {
+			return nil, err
+		}
+		reqBody.ContentType = "application/json"
+		reqBody.Value = bodyStr.GetValue()
+	}
+
+	allowRedirectValue := allowRedicrect.GetValue()
+	timeoutValue := timeout.GetValue()
+
+	// build request
+	req, err := util.BuildBaseHttpRequest(
+		pathValue,
+		methodValue,
+		headersValue,
+		queryParamValue,
+		reqBody,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// sendRequest
+	resp, data, err := util.SendHttpRequest(req, allowRedirectValue, int(timeoutValue))
+	if err != nil {
+		return nil, err
+	}
+
+	// build HTTP响应 object
+	initProps := map[string]r.Element{
+		"状态码": value.NewNumber(float64(resp.StatusCode)),
+		"内容":  value.NewString(string(data)),
+	}
+	return value.NewObject(CLASS_HttpResposne, initProps), nil
+}
 
 // HTTP响应类型
 /**
