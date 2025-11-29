1377b2436ef7f1edfccae443eb7f4e7119e4cde4
DemoHn <demohn2016@gmail.com>
2025-11-29 17:27:42 +0800

    feat: optimize

--- stdlib/http/http_helper.go
+++ stdlib/http/http_helper.go
@@ -19,6 +19,104 @@ type ReqBody struct {
 	Value       string
 }
 
+// build *http.Request from HTTP请求 Element
+func digestBaseRequest(reqElement r.Element) (*libHTTP.Request, error) {
+	reqObj, err0 := value.AssertElement[*value.Object](reqElement)
+	if err0 != nil {
+		return nil, err0
+	}
+	if !reqObj.IsInstanceOf(common.CLASS_HttpRequest) {
+		return nil, value.ThrowException("不匹配的参数类型：HTTP请求")
+	}
+
+	path, err1 := value.AssertPropertyElement[*value.String](reqObj, "路径")
+	method, err2 := value.AssertPropertyElement[*value.String](reqObj, "方法")
+	headers, err3 := value.AssertPropertyElement[*value.HashMap](reqObj, "头部")
+	// assert String or HashMap
+	queryParam, err4 := value.BuildEitherPropertyElement[*value.String, *value.HashMap](reqObj, "查询参数")
+	// assert String or HashMap
+	body, err5 := value.BuildEitherPropertyElement[*value.String, *value.HashMap](reqObj, "内容")
+
+	// assert errors
+	for _, e := range []error{err1, err2, err3, err4, err5} {
+		if e != nil {
+			return nil, e
+		}
+	}
+
+	// get params
+	pathValue := path.GetValue()
+	methodValue := method.GetValue()
+
+	// get headers
+	headersValue := make([][2]string, 0)
+	for k, v := range headers.GetValue() {
+		// only string Value is allowed to append into header set
+		// others will be filted without any warning wor~
+		if str, ok := v.(*value.String); ok {
+			headersValue = append(headersValue, [2]string{k, str.GetValue()})
+		}
+	}
+
+	// get query param
+	queryParamValue := make([][2]string, 0)
+	if queryParam.IsA() { // A:*String, B:*HashMap
+		qs := queryParam.GetA().GetValue()
+		qsValue, err := libURL.ParseQuery(qs)
+		if err != nil {
+			return nil, err
+		}
+		for k, v := range qsValue {
+			for _, item := range v {
+				queryParamValue = append(queryParamValue, [2]string{k, item})
+			}
+		}
+	} else {
+		// from HashMap -> map[string][]string
+		for k, v := range queryParam.GetB().GetValue() {
+			switch vv := v.(type) {
+			case *value.String:
+				queryParamValue = append(queryParamValue, [2]string{k, vv.GetValue()})
+			// handle Array of String
+			case *value.Array:
+				for _, inItem := range vv.GetValue() {
+					if inItemStr, ok := inItem.(*value.String); ok {
+						queryParamValue = append(queryParamValue, [2]string{k, inItemStr.GetValue()})
+					}
+				}
+			}
+		}
+	}
+
+	// convert reqBody
+	var reqBody ReqBody
+	if body.IsA() { // A:*String, B:*HashMap
+		reqBody.ContentType = "application/x-www-form-urlencoded"
+		reqBody.Value = body.GetA().GetValue()
+	} else {
+		bodyStr, err := common.HashMapToJSONString(body.GetB())
+		if err != nil {
+			return nil, err
+		}
+		reqBody.ContentType = "application/json"
+		reqBody.Value = bodyStr.GetValue()
+	}
+
+	// build request
+	return buildBaseHttpRequest(
+		pathValue,
+		methodValue,
+		headersValue,
+		queryParamValue,
+		reqBody,
+	)
+}
+
+// accept HashMap or String
+func digestReqBody(reqBodyElement r.Element) (ReqBody, error) {
+
+}
+
 func buildBaseHttpRequest(
 	url string,
 	method string,
